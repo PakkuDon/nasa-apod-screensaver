fe55227cbd7768c2372b7118d44e7973608e3c0f
PakkuDon <pakku.pd@gmail.com>
2017-04-25 18:04:33 +1000

    Add Redux actions

---
+++ src/actions.js
@@ -0,0 +1,29 @@
+import {
+  FETCH_IMAGES_REQUEST,
+  FETCH_IMAGES_START,
+  FETCH_IMAGES_SUCCESS,
+  FETCH_IMAGES_FAIL,
+  CHANGE_IMAGE,
+} from './constants';
+
+export const fetchImages = () => ({
+  type: FETCH_IMAGES_REQUEST
+})
+
+export const fetchImagesStart = () => ({
+  type: FETCH_IMAGES_START
+})
+
+export const fetchImagesSuccess = images => ({
+  type: FETCH_IMAGES_SUCCESS,
+  images
+})
+
+export const fetchImagesFail = error => ({
+  type: FETCH_IMAGES_FAIL,
+  error
+})
+
+export const changeImage = () => ({
+  type: CHANGE_IMAGE
+})
