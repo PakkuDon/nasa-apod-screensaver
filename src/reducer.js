dc7a146eabd98eaf6ba52ff13ea6aa1a234121a3
PakkuDon <pakku.pd@gmail.com>
2017-04-25 23:43:19 +1000

    Add reducer

---
+++ src/reducer.js
@@ -0,0 +1,32 @@
+import * as Actions from './constants';
+
+const initialState = {
+  images: [],
+  index: 0,
+  loading: false
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case Actions.FETCH_IMAGES_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case Actions.FETCH_IMAGES_SUCCESS:
+      return {
+        ...state,
+        images: action.images,
+        loading: false
+      }
+    case Actions.FETCH_IMAGES_FAIL:
+      return {
+        ...state,
+        error: action.error,
+        loading: false
+      }
+    case Actions.CHANGE_IMAGE:
+    default:
+      return state;
+  }
+}
